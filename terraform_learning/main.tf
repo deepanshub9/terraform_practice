7b432f9a22cdb310667446b7eef48e237e46d1ac
deepanshub9 <kukukumar1996@gmial.com>
2026-07-26 17:58:18 +0100

    Vnet module add

--- terraform_learning/main.tf
+++ terraform_learning/main.tf
@@ -7,3 +7,16 @@ module "resource_group" {
   location = var.location
 
 }
+module "network" {
+
+  source = "./modules/network"
+
+  vnet_name = var.vnet_name
+
+  resource_group_name = module.resource_group.resource_group_name
+
+  location = var.location
+
+  address_space = var.address_space
+
+}
--- terraform_learning/modules/network/variables.tf
+++ terraform_learning/modules/network/variables.tf
@@ -1,11 +1,12 @@
+variable "vnet_name" {
+  type = string
+}
+
 variable "resource_group_name" {
   type = string
 }
 
 variable "location" {
-  type = string
-}
-variable "vnet_name" {
   type = string
 }
 
